e1b7ec86d3003a16581878eb2bb503fef7bd5080
frenzyali <syedpheonixhussain@gmail.com>
2024-03-27 18:02:49 +0500

    factorialCalculator

---
+++ FactorialCalculator/index.ts
@@ -0,0 +1,47 @@
+// Factorial Calculator by Syed Ali Hussain
+
+import inquirer from "inquirer";
+
+function factorial(n: number): number{
+    if(n === 0){          // If the input given is 0 then it's factorial will be 1
+        return 1
+    }else{
+        return factorial(n-1) * n;   
+    }
+    
+        
+}
+let isRunning = true;
+START:
+while(isRunning){
+    let answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "option",
+            message: "> Enter a non negative integer you want to get the factorial of: ",
+        }
+    ])
+    if(answer.option >= 0 || typeof(answer.option) != typeof(1)){
+        console.log(`> The factorial of ${answer.option} is: ` + factorial(answer.option));
+    }else{
+        console.log("Invalid input")
+        continue START;
+    }
+    let answer2 = await inquirer.prompt([
+        {
+            type: "list",
+            name: "option",
+            message: "> What do you want to do?",
+            choices: ["Exit", "Continue"]
+        }
+    ])
+    if(answer2.option == "Exit"){
+        console.clear()
+        console.log("> Goodbye");
+        isRunning = false;
+    }else if(answer2.option == "Continue"){
+        console.clear()
+        continue START;
+    }
+}
+
