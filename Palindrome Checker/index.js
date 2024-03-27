3275df0e5c4e95997eeef7f0c38b53f7bb470f25
frenzyali <syedpheonixhussain@gmail.com>
2024-03-27 14:18:22 +0500

    palindromeChecker

---
+++ Palindrome Checker/index.js
@@ -0,0 +1,22 @@
+// Palindrome Checker By Syed Ali Hussain
+import inquirer from "inquirer";
+function palindromeCheck(word) {
+    let stringtoArray = word.split(''); // Changes my input string to Array
+    let reversedArray = stringtoArray.reverse(); // Reverses the order of Array 
+    let answer = reversedArray.join(''); // Joins the elements of the array to make a string
+    // Conditional Statements 
+    if (answer.toLowerCase() == word.toLowerCase()) {
+        console.log("It is a Palindrome");
+    }
+    else {
+        console.log("It is not a Palindrome");
+    }
+}
+let answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "word",
+        message: "Enter your word: "
+    }
+]);
+palindromeCheck(answer.word);
